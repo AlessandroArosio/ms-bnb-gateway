b21996e1ae241d7544375915d119718907507d8b
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-12-23 16:47:42 +0000

    Adding base project

---
+++ src/main/java/com/aledev/alba/msbnbgateway/MsBnbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBnbGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBnbGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aledev/alba/msbnbgateway/MsBnbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBnbGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
